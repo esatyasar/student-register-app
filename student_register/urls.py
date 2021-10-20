91d4602afdc5d9c90635e97d48f5cbafb4aa5c00
esatyasar <esatyasar91@gmail.com>
2021-10-20 14:18:35 +0300

    added student_form

--- student_register/urls.py
+++ student_register/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
+from django.urls import path
+from .views import student_form
 
 urlpatterns = [
-    
+    path('student_form/', student_form, name = "form" ),
 ]
--- student_register/views.py
+++ student_register/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def student_form(request):
+    return render (request, "student_register/student_form.html")
