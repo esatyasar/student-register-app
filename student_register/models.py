3fc41893ed4de57d584dacb3f7f5b7208e8fecaa
esatyasar <esatyasar91@gmail.com>
2021-10-20 13:52:49 +0300

    created models

--- student_register/models.py
+++ student_register/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+
+class Register(models.Model):
+    
+    name = models.CharField(max_length=30)
+    email = models.EmailField(max_length=154)
+    phone = models.CharField(max_length=11)
+    number = models.IntegerField()
+    
+    GENDER = (
+        ("1", "Female"),
+        ("2", "Male"),
+        ("3", "Other"),
+        ("4", "Prefer Not Say"),
+    )
+    gender = models.CharField(max_length = 50, choices=GENDER)
+    
+    PATH = (
+        ("1", "Full Stack Developer"),
+        ("2", "Data Science"),
+        ("3", "AWS")
+    )
+
+    path = models.CharField(max_length = 50, choices=PATH)
