d6425c1ca83a713fa3dc692259d80aaeb4e958c0
esatyasar <esatyasar91@gmail.com>
2021-10-20 14:42:53 +0300

    added form

--- student_register/views.py
+++ student_register/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .forms import Register_form
 
 # Create your views here.
 def student_form(request):
-    return render (request, "student_register/student_form.html")
+    form = Register_form()
+    context = {
+        "form" : form
+    }
+    return render (request, "student_register/student_form.html",context)
